e4d1a81bb5e2241dad20eeaa3c05f19a90594ace
flora chen <flora1591@hotmail.com>
2024-07-24 13:08:56 -0700

    created dashboard, login, and registration pages with href

--- cailorie/src/components/RegisterForm.jsx
+++ cailorie/src/components/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function LoginForm() {
+export default function RegisterForm() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -11,6 +11,12 @@ export default function LoginForm() {
           <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="#" method="POST">
               <div>
+                <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div class="mt-2">
+                  <input id="name" name="name" type="name" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+              </div>
+              <div>
                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                 <div class="mt-2">
                   <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
@@ -20,17 +26,14 @@ export default function LoginForm() {
               <div>
                 <div class="flex items-center justify-between">
                   <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                  <div class="text-sm">
-                    <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                  </div>
                 </div>
                 <div class="mt-2">
                   <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 </div>
               </div>
               <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <Link href='/dashboard' class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Register here!</Link>
+                Already have an account? 
+                <Link href='/' class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Login!</Link>
               </p>
               <div>
                 <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
