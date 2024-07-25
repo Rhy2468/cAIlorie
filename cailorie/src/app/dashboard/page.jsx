d41cfb431592a4bf347718e9f36c6cd0ba49acf0
Ricky Yang <rhy2@sfu.ca>
2024-07-24 21:57:44 -0700

    Merging

--- cailorie/src/app/dashboard/page.jsx
+++ cailorie/src/app/dashboard/page.jsx
@@ -20,6 +20,20 @@ const userNavigation = [
     { name: 'Sign out', href: '#' },
 ]
 
+const links = [
+    { name: 'Open roles', href: '#' },
+    { name: 'Internship program', href: '#' },
+    { name: 'Our values', href: '#' },
+    { name: 'Meet our leadership', href: '#' },
+]
+
+const stats = [
+    { name: 'Offices worldwide', value: '12' },
+    { name: 'Full-time colleagues', value: '300+' },
+    { name: 'Hours per week', value: '40' },
+    { name: 'Paid time off', value: 'Unlimited' },
+  ]
+
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
